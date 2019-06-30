032ddfced1ddaca151bcb06d72d7eaeb847d4b2f
Jason Belcher <jason.belcher@cumberlandgroupit.com>
2019-06-30 19:33:13 -0400

    convert to front end code

--- drum-machine.js
+++ drum-machine.js
@@ -1,4 +1,33 @@
-let player = require("play-sound")();
+var kick = new Howl({
+  src: [
+    "http://localhost:8080/samples/Deep%20House%20Drum%20Samples/bd_kick/bd_909dwsd.wav"
+  ]
+});
+var clap = new Howl({
+  src: [
+    "http://localhost:8080/samples/Deep%20House%20Drum%20Samples/clap/clp_analogue.wav"
+  ]
+});
+var hat = new Howl({
+  src: [
+    "http://localhost:8080/samples/Deep%20House%20Drum%20Samples/hats/hat_darkstar.wav"
+  ]
+});
+var shaker = new Howl({
+  src: [
+    "http://localhost:8080/samples/Deep%20House%20Drum%20Samples/shaker_tambourine/shaker_quicky.wav"
+  ]
+});
+var bongo1 = new Howl({
+  src: [
+    "http://localhost:8080/samples/Deep%20House%20Drum%20Samples/percussion/prc_bongodrm.wav"
+  ]
+});
+var congaz = new Howl({
+  src: [
+    "http://localhost:8080/samples/Deep%20House%20Drum%20Samples/percussion/prc_congaz.wav"
+  ]
+});
 
 // For each tick in a sequence this object will be checked
 // to determine what instruments need
@@ -9,33 +38,38 @@ function DrumMachineState(id) {
   this.kick = {
     on: false,
     name: "kick",
-    location: "./samples/Deep House Drum Samples/bd_kick/bd_deephouser.wav"
+    volume: volume => (kick._volume = volume),
+    play: () => kick.play()
   };
   this.clap = {
     on: false,
     name: "clap",
-    location: "./samples/Deep House Drum Samples/clap/clp_analogue.wav"
+    volume: volume => (clap._volume = volume),
+    play: () => clap.play()
   };
   this.hat = {
     on: false,
     name: "hat",
-    location: "./samples/Deep House Drum Samples/hats/hat_analog.wav"
+    volume: volume => (hat._volume = volume),
+    play: () => hat.play()
   };
   this.shaker = {
     on: false,
     name: "shaker",
-    location:
-      "./samples/Deep House Drum Samples/shaker_tambourine/shaker_bot.wav"
+    volume: volume => (shaker._volume = volume),
+    play: () => shaker.play()
   };
   this.perc = {
     on: false,
-    name: "perc",
-    location: "./samples/Deep House Drum Samples/percussion/prc_bongodrm.wav"
+    name: "perc1",
+    volume: volume => (bongo1._volume = volume),
+    play: () => bongo1.play()
   };
   this.perc2 = {
     on: false,
     name: "perc2",
-    location: "./samples/Deep House Drum Samples/percussion/prc_congaz.wav"
+    volume: volume => (congaz._volume = volume),
+    play: () => congaz.play()
   };
 }
 
@@ -44,7 +78,7 @@ DrumMachineState.prototype.getState = function(name) {
 };
 DrumMachineState.prototype.setState = function(name, on, volume) {
   this[name].on = on;
-  this[name].volume = volume || 1;
+  this[name].volume(volume) || this[name].volume(1);
   this[name].name = name;
 };
 
@@ -109,10 +143,8 @@ const dm = {
     for (let instrument in dmState) {
       if (dmState.hasOwnProperty(instrument)) {
         if (dmState[instrument].on) {
-          console.log(dmState.getState(instrument).location);
-          player.play(dmState.getState(instrument).location, {
-            afplay: ["-v", dmState.getState(instrument).volume]
-          });
+          console.log("triggered:  ", dmState[instrument].name);
+          dmState.getState(instrument).play();
         }
       }
     }
@@ -121,10 +153,10 @@ const dm = {
 
 let sequencer = new Sequencer(DrumMachineState, 8);
 let seq = sequencer.initSeq();
-
 seq[0].setState("kick", true, 1);
 seq[4].setState("kick", true, 0.8);
-seq[4].setState("clap", true);
+seq[4].setState("clap", true, 0.5);
+
 seq[2].setState("hat", true, 0.2);
 seq[6].setState("hat", true, 0.24);
 seq[0].setState("shaker", true, 0.2);
@@ -142,32 +174,5 @@ seq[2].setState("perc2", true, 0.3);
 seq[3].setState("perc2", true, 0.5);
 seq[7].setState("perc2", true, 0.25);
 
-dm.setTempo(127, 4);
-dm.start(seq);
-
-dm.stop(4000).then(() => {
-  seq[0].setState("kick", true, 1);
-  seq[4].setState("kick", true, 0.8);
-  seq[4].setState("clap", true);
-  seq[2].setState("hat", true, 0.2);
-  seq[6].setState("hat", true, 0.24);
-  seq[0].setState("shaker", true, 0.2);
-  seq[1].setState("shaker", true, 0.1);
-  seq[2].setState("shaker", true, 0.2);
-  seq[3].setState("shaker", true, 0.1);
-  seq[4].setState("shaker", true, 0.2);
-  seq[5].setState("shaker", true, 0.1);
-  seq[6].setState("shaker", true, 0.2);
-  seq[7].setState("shaker", true, 0.1);
-  seq[1].setState("perc", true, 0.3);
-  seq[2].setState("perc", true, 0.4);
-  seq[0].setState("perc", true, 0.3);
-  seq[4].setState("perc2", true, 0.3);
-  seq[0].setState("perc2", true, 0.5);
-  seq[6].setState("perc2", true, 0.25);
-
-  dm.setTempo(127, 2);
-
-  dm.start(seq);
-  dm.stop(4000);
-});
+dm.setTempo(120, 2000);
+dm.start(seq, 2000);
